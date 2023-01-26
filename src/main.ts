ad7813ec9b8ad9d835535e8a808a2fd265f0ec77
Jon McMillan <mcmillan.jon@gmail.com>
2023-01-26 16:51:25 +0100

    Working

--- src/main.ts
+++ src/main.ts
@@ -1,88 +1,118 @@
 import "./style.css";
-import initSqlJs, { SqlJsStatic } from "sql.js/dist/sql-wasm.js";
-
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-    <h1>File System Access API</h1>
-    <div class="card">
-      <button id="counter" type="button">Get Access</button>
-      <button id="download" type="button">Download</button>
-    </div>
-    <div>
-      <h2>Database</h2>
-      <div id="db"></div>
-    </div>
-  </div>
-`;
+import initSqlJs, {
+  Database,
+  QueryExecResult,
+  SqlJsStatic,
+} from "sql.js/dist/sql-wasm.js";
 
 let dirHandle: FileSystemDirectoryHandle | null = null;
+let fileHandle: FileSystemFileHandle | null = null;
 let SQL: SqlJsStatic;
+let DB: Database;
 const id = "simrex-id";
 
-const requestPermission = (button: HTMLButtonElement) => {
-  button.addEventListener("click", async () => {
-    if (!dirHandle) {
-      dirHandle = await window.showDirectoryPicker({
-        id,
-        startIn: "downloads",
-      });
-    }
+const writeTable = (result: QueryExecResult) => {
+  const str = `<table>
+  <thead>
+    <tr>
+      ${result.columns.map((col) => `<th>${col}</th>`).join("")}
+      </tr>
+  </thead>
+  <tbody>
+    ${result.values
+      .map((row) => `<tr>${row.map((col) => `<td>${col}</td>`).join("")}</tr>`)
+      .join("")}
+  </tbody>
+</table>`;
 
-    const entries = await dirHandle.entries();
-    for await (const [name, handle] of entries) {
-      if (handle.kind === "file") {
-        if (name.endsWith(".db")) {
-          const file = await handle.getFile();
-          const ab = await file.arrayBuffer();
-          const buffer = new Uint8Array(ab);
-
-          const db = new SQL.Database(buffer);
-          const tables = db.exec("SELECT * FROM sqlite_schema").at(0);
-          const str = `<table>
-            <thead>
-              <tr>
-                ${tables.columns.map((col) => `<th>${col}</th>`).join("")}
-                </tr>
-            </thead>
-            <tbody>
-              ${tables.values
-                .map(
-                  (row) =>
-                    `<tr>${row.map((col) => `<td>${col}</td>`).join("")}</tr>`
-                )
-                .join("")}
-            </tbody>
-          </table>`;
-          document.querySelector<HTMLDivElement>("#db")!.innerHTML = str;
-          console.log(db.exec("SELECT * FROM sqlite_schema"));
-        }
-      }
-    }
-  });
+  document.querySelector<HTMLDivElement>("#db")!.innerHTML = str;
 };
 
-const setupDownload = (button: HTMLButtonElement) => {
-  button.addEventListener("click", async () => {
-    if (!dirHandle) {
-      return;
-    }
-
-    const fileHandle = await dirHandle.getFileHandle("test.txt", {
-      create: true,
+const loadDatabase = async () => {
+  if (!dirHandle) {
+    dirHandle = await window.showDirectoryPicker({
+      id,
+      startIn: "downloads",
     });
+  }
 
-    const writable = await fileHandle.createWritable();
-    await writable.write("Hello World");
-    await writable.close();
-  });
+  const entries = await dirHandle.entries();
+  for await (const [name, handle] of entries) {
+    if (handle.kind === "file" && name.endsWith(".db")) {
+      fileHandle = handle;
+      const file = await handle.getFile();
+      const ab = await file.arrayBuffer();
+      const buffer = new Uint8Array(ab);
+
+      DB = new SQL.Database(buffer);
+      const tables = DB.exec("SELECT * FROM sqlite_schema").at(0);
+      tables && writeTable(tables);
+    }
+  }
+};
+
+const createTable = async () => {
+  if (!DB) {
+    return;
+  }
+
+  DB.run("CREATE TABLE test (name TEXT)");
+};
+
+// Reads from the test table and displays the rows in a table
+const readTable = async () => {
+  if (!DB) {
+    return;
+  }
+
+  const result = DB.exec("SELECT * FROM test");
+  result && writeTable(result.at(0));
+};
+
+// Add a row to the test table and refresh the table
+const addRow = async () => {
+  if (!DB) {
+    return;
+  }
+
+  DB.run("INSERT INTO test VALUES ('test from the website')");
+  readTable();
+};
+
+// save the database back to the file system
+const save = async () => {
+  if (!fileHandle || !DB) {
+    return;
+  }
+
+  const writable = await fileHandle.createWritable();
+  await writable.write(DB.export());
+  await writable.close();
 };
 
 const setupSql = async () => {
   SQL = await initSqlJs({
-    locateFile: (file) => `/sql-wasm.wasm`,
+    locateFile: () => `/sql-wasm.wasm`,
   });
 };
 
-requestPermission(document.querySelector<HTMLButtonElement>("#counter")!);
-setupDownload(document.querySelector<HTMLButtonElement>("#download")!);
+document
+  .querySelector<HTMLButtonElement>("#load")
+  ?.addEventListener("click", loadDatabase);
+
+document
+  .querySelector<HTMLButtonElement>("#create-table")
+  ?.addEventListener("click", createTable);
+
+document
+  .querySelector<HTMLButtonElement>("#add-row")
+  ?.addEventListener("click", addRow);
+
+document
+  .querySelector<HTMLButtonElement>("#read-table")
+  ?.addEventListener("click", readTable);
+
+document
+  .querySelector<HTMLButtonElement>("#save")
+  ?.addEventListener("click", save);
 setupSql();
